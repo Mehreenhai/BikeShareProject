00da274ba671ac4c9400aba6a1771d22fe2bba57
Nicki Lang <pandaunderwear@gmail.com>
2018-03-08 14:57:00 -0800

    Added more timeslider code, commented it out on app.js for now.

--- js/app.js
+++ js/app.js
@@ -65,3 +65,111 @@ d3.csv(stationsCSV, function(error, response) {
 
 
 });
+
+// mapboxgl.accessToken = 'pk.eyJ1IjoibGl0dGxlc3Rkb2xsIiwiYSI6ImNqZHdnbTBzYTQ3bXUyeG80ZTQ3dWJtNjIifQ.uvSL6xgyBBXQSJ1Yopx9gA';
+// var map = new mapboxgl.Map({
+//     container: 'map',
+//     style: 'mapbox://styles/mapbox/light-v9',
+//     center: [31.4606, 20.7927], //need to change coordinates
+//     zoom: 0.5
+// });
+
+// var months = [ //would we want year data?
+//     'January',
+//     'February',
+//     'March',
+//     'April',
+//     'May',
+//     'June',
+//     'July',
+//     'August',
+//     'September',
+//     'October',
+//     'November',
+//     'December'
+// ];
+
+// function filterBy(month) {
+
+//     var filters = ['==', 'month', month];
+//     map.setFilter('station-circles', filters);
+//     map.setFilter('station-labels', filters);
+
+//     // Set the label to the month
+//     document.getElementById('month').textContent = months[month];
+// }
+
+// map.on('load', function() {
+
+//     // Data courtesy of http://earthquake.usgs.gov/
+//     // Query for significant earthquakes in 2015 URL request looked like this:
+//     // http://earthquake.usgs.gov/fdsnws/event/1/query
+//     //    ?format=geojson
+//     //    &starttime=2015-01-01
+//     //    &endtime=2015-12-31
+//     //    &minmagnitude=6'
+//     //
+//     // Here we're using d3 to help us make the ajax request but you can use
+//     // Any request method (library or otherwise) you wish.
+//     d3.json('/mapbox-gl-js/assets/significant-earthquakes-2015.geojson', function(err, data) {
+//         if (err) throw err;
+
+//         // Create a month property value based on time
+//         // used to filter against.
+//         data.features = data.features.map(function(d) {
+//             d.properties.month = new Date(d.properties.time).getMonth();
+//             return d;
+//         });
+
+//         map.addSource('stations', {
+//             'type': 'geojson',
+//             'data': data
+//         });
+
+//         map.addLayer({
+//             'id': 'station-circles',
+//             'type': 'circle',
+//             'source': 'stations',
+//             'paint': {
+//                 'circle-color': [
+//                     'interpolate',
+//                     ['linear'],
+//                     ['get', 'mag'],
+//                     6, '#FCA107',
+//                     8, '#7F3121'
+//                 ],
+//                 'circle-opacity': 0.75,
+//                 'circle-radius': [
+//                     'interpolate',
+//                     ['linear'],
+//                     ['get', 'mag'],
+//                     6, 20,
+//                     8, 40
+//                 ]
+//             }
+//         });
+
+//         map.addLayer({
+//             'id': 'station-labels',
+//             'type': 'symbol',
+//             'source': 'stations',
+//             'layout': {
+//                 'text-field': ['concat', ['to-string', ['get', 'mag']], 'm'],
+//                 'text-font': ['Open Sans Bold', 'Arial Unicode MS Bold'],
+//                 'text-size': 12
+//             },
+//             'paint': {
+//                 'text-color': 'rgba(0,0,0,0.5)'
+//             }
+//         });
+
+//         // Set filter to first month of the year
+//         // 0 = January
+//         filterBy(0);
+
+//         document.getElementById('slider').addEventListener('input', function(e) {
+//             var month = parseInt(e.target.value, 10);
+//             filterBy(month);
+//         });
+//     });
+// });
